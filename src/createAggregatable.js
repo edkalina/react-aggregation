53e6aae5ece6e5b0cdf0ba6439f4a6ac00072e49
Gordei Kalina <gordei@kalina.red>
2018-06-08 07:21:21 +0300

    Make Aggregator availaible for AggregatableWithFallback too

--- src/createAggregatable.js
+++ src/createAggregatable.js
@@ -6,20 +6,24 @@ import AggregationContext from './AggregationContext';
 
 export default function createAggregatable() {
   const id = {};
+  const Aggregator = createAggregator(id);
   const Aggregatable = props => <Holder data={props} id={id} />;
 
-  Aggregatable.Aggregator = createAggregator(id);
+  Aggregatable.Aggregator = Aggregator;
 
-  Aggregatable.withFallback = fallback =>
+  Aggregatable.withFallback = fallback => {
     function AggregatableWithFallback(props) {
       return (
         <AggregationContext.Consumer>
-          {inAggregation =>
-            inAggregation ? <Holder data={props} id={id} /> : fallback(props)
-          }
+          {inAggregation => (inAggregation ? <Holder data={props} id={id} /> : fallback(props))}
         </AggregationContext.Consumer>
       );
-    };
+    }
+
+    AggregatableWithFallback.Aggregator = Aggregator;
+
+    return AggregatableWithFallback;
+  };
 
   return Aggregatable;
 }
